baa56b2533a83fb0f45bfe9bbae1d0e35f424d3e
Daniel Erazo <danielmauricioerazoespinoza@gmail.com>
2025-10-23 13:01:06 -0500

    Min confidence removed

--- src/utils/rag_utils.py
+++ src/utils/rag_utils.py
@@ -7,7 +7,6 @@ load_dotenv()
 retriever = AmazonKnowledgeBasesRetriever(
     knowledge_base_id="EOYQCYSEAB",
     retrieval_config={"vectorSearchConfiguration": {"numberOfResults": 4}},
-    min_score_confidence=0.5,
     region_name="us-east-2"
 )
 
@@ -16,16 +15,6 @@ retriever_tool = create_retriever_tool(
     "retrieve_prodcuts_and_services_information",
     "Search and return information about products or services."
 )
-
-# try:
-#     docs = retriever.invoke("políticas de reembolso y garantía para teléfonos dañados")
-#     print("Docs:", docs)
-# except Exception as e:
-#     import traceback
-#     traceback.print_exc()
-#     print("Exception type:", type(e))
-#     # if botocore exception, show .response if present
-#     print("Exception attrs:", getattr(e, "response", None))
 
 def get_retriever_tool():
     return retriever_tool
